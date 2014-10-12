d979222cd463a204bad7ab45543af2a2d0465411
blm95 <blm95@comcast.net>
2014-10-12 12:14:04 -0400

    VETERAN MODE - No Evade, must run this assembly the entire game

---
+++ VETERAN MODE/VETERAN MODE/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace VETERAN_MODE
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+        }
+
+        private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
+        {
+            var c = BitConverter.ToString(args.PacketData);
+            if(c[0].ToString() + c[1].ToString() == "87")
+                args.Process = false;
+        }
+    }
+}
