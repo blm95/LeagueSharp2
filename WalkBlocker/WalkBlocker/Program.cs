78c845220cf9482deffa2b38ff7c51d1afec44cf
blm95 <blm95@comcast.net>
2014-10-25 12:25:18 -0400

    Safe Zone commit

---
+++ WalkBlocker/WalkBlocker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp.Common;
+//using Test = Test.Test;
+
+namespace WalkBlocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Test.Test.onGameLoad;
+        }
+    }
+}
