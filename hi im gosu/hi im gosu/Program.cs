7475e8ce924c5fe4bc40a57dd294a07b14491256
blm95 <blm95@comcast.net>
2015-01-21 18:07:51 -0500

    items, cleanse, some more stuff

--- hi im gosu/hi im gosu/Program.cs
+++ hi im gosu/hi im gosu/Program.cs
@@ -25,7 +25,12 @@ namespace hi_im_gosu
         public static Obj_AI_Hero tar;
         public const string ChampName = "Vayne";
         public static Obj_AI_Hero Player;
-        
+        private static BuffType[] buffs;
+        private static Spell cleanse;
+        private static Menu Itemsmenu;
+        private static Menu botrk;
+        private static Menu qss;
+
 
         static void Main(string[] args)
         {
@@ -55,9 +60,11 @@ namespace hi_im_gosu
             //TS
             var TargetSelectorMenu = new Menu("Target Selector", "Target Selector");
             TargetSelector.AddToMenu(TargetSelectorMenu);
-           // SimpleTs.AddToMenu(TargetSelectorMenu);
+            // SimpleTs.AddToMenu(TargetSelectorMenu);
             menu.AddSubMenu(TargetSelectorMenu);
 
+            menu.AddItem(
+                new MenuItem("aaqaa", "Auto -> Q -> AA").SetValue(new KeyBind("X".ToCharArray()[0], KeyBindType.Press)));
             //menu.AddSubMenu(new Menu("Combo", "combo"));
             //menu.SubMenu("combo").AddItem(new MenuItem("laugh", "Cancel w/ Laugh")).SetValue(false);
             menu.AddItem(
@@ -73,6 +80,28 @@ namespace hi_im_gosu
             menu.AddSubMenu(new Menu("Gapcloser List", "gap"));
             menu.AddSubMenu(new Menu("Gapcloser List 2", "gap2"));
             menu.AddSubMenu(new Menu("Interrupt List", "int"));
+
+            Itemsmenu = menu.AddSubMenu(new Menu("Items", "Items"));
+            Itemsmenu.AddItem(new MenuItem("BOTRK", "Use BOTRK").SetValue(true));
+            botrk = Itemsmenu.AddSubMenu(new Menu("BOTRK Settings", "usebotrk"));
+            botrk.AddItem(new MenuItem("myhp", "Use if my HP < %")).SetValue(new Slider(20, 0, 100));
+            botrk.AddItem(new MenuItem("theirhp", "Use if enemy HP < %")).SetValue(new Slider(20, 0, 100));
+            Itemsmenu.AddItem(new MenuItem("Ghostblade", "Use Ghostblade").SetValue(true));
+            Itemsmenu.AddItem(new MenuItem("QSS", "Use QSS/Merc Scimitar/Cleanse").SetValue(true));
+            qss = menu.SubMenu("Items").AddSubMenu(new Menu("QSS/Merc/Clease Settings", "useqss"));
+
+            buffs = new[]
+            {
+                BuffType.Blind, BuffType.Charm, BuffType.CombatDehancer, BuffType.Fear, BuffType.Flee, BuffType.Knockback,
+                BuffType.Knockup, BuffType.Polymorph, BuffType.Silence, BuffType.Sleep, BuffType.Snare, BuffType.Stun,
+                BuffType.Suppression, BuffType.Taunt
+            };
+
+            for (int i=0; i < buffs.Length;i++)
+            {
+               qss.AddItem(new MenuItem(buffs[i].ToString(), buffs[i].ToString()).SetValue(true));
+            }
+            
             Q = new Spell(SpellSlot.Q, 0f);
             E = new Spell(SpellSlot.E, float.MaxValue);
 
@@ -106,6 +135,15 @@ namespace hi_im_gosu
             {
                 menu.SubMenu("int").AddItem(new MenuItem(interrupt[i], interrupt[i])).SetValue(true);
             }
+
+            var cde = ObjectManager.Player.Spellbook.GetSpell(ObjectManager.Player.GetSpellSlot("SummonerBoost"));
+            if (cde != null)
+            {
+                if (cde.Slot != SpellSlot.Unknown)  //trees
+                {
+                    cleanse = new Spell(cde.Slot);
+                }
+            }
             menu.AddSubMenu(new Menu("Harass Options", "harass"));
             menu.SubMenu("harass").AddItem(new MenuItem("hq", "Use Q Harass").SetValue(true));
             menu.SubMenu("harass").AddItem(new MenuItem("he", "Use E Harass").SetValue(true));
@@ -118,17 +156,19 @@ namespace hi_im_gosu
 
         public static void Game_ProcessSpell(Obj_AI_Base hero, GameObjectProcessSpellCastEventArgs args)
         {
-            if (menu.Item("UseEInterrupt").GetValue<bool>() && hero.IsValidTarget(550f) &&
+            //Game.PrintChat(args.SData.Name);
+            if (menu.Item("UseEInterrupt").GetValue<bool>() &&
                 menu.Item(args.SData.Name).GetValue<bool>())
             {
-                if (interrupt.Any(str => str.Contains(args.SData.Name)))
-                {
+               // Game.PrintChat("in");
+                //if (interrupt.Any(str => str.Contains(args.SData.Name)))
+                //{
+                  //  Game.PrintChat("in2");
                     E.Cast(hero);
-                }
+                //}
             }
 
-            if (gapcloser.Any(str => str.Contains(args.SData.Name)) && args.Target == ObjectManager.Player &&
-                hero.IsValidTarget(550f) && menu.Item(args.SData.Name).GetValue<bool>())
+            if (gapcloser.Any(str => str.Contains(args.SData.Name)) && menu.Item(args.SData.Name).GetValue<bool>())
             {
                 E.Cast(hero);
             }
@@ -143,69 +183,117 @@ namespace hi_im_gosu
 
         public static void Orbwalking_AfterAttack(AttackableUnit unit, AttackableUnit target)
         {
+            if (!unit.IsMe) return;
+            if (!(target is Obj_AI_Hero)) return;
 
-            if (unit.IsMe)
+            tar = (Obj_AI_Hero)target;
+
+            if (menu.Item("aaqaa").GetValue<KeyBind>().Active)
             {
-                tar = (Obj_AI_Hero) target;
-
-                if (menu.Item("UseEaa").GetValue<KeyBind>().Active)
+                if (Q.IsReady())
                 {
-                    E.Cast((Obj_AI_Base)target);
-                    menu.Item("UseEaa").SetValue<KeyBind>(new KeyBind("G".ToCharArray()[0], KeyBindType.Toggle));
+                    Q.Cast(Game.CursorPos);
                 }
-
-                if (((orbwalker.ActiveMode.ToString() == "Combo" && menu.Item("UseQC").GetValue<bool>()) || (orbwalker.ActiveMode.ToString() == "Mixed" && menu.Item("hq").GetValue<bool>()) &&
-                    Q.IsReady()))
-                {
-                    var after = ObjectManager.Player.Position +
-                                Normalize(Game.CursorPos - ObjectManager.Player.Position)*300;
-                    //Game.PrintChat("After: {0}", after);
-                    var disafter = Vector3.DistanceSquared(after, tar.Position);
-                    //Game.PrintChat("DisAfter: {0}", disafter);
-                    //Game.PrintChat("first calc: {0}", (disafter) - (630*630));
-                    if ((disafter < 630*630) && disafter > 150*150)
-                    {
-                        Q.Cast(Game.CursorPos);
-                    }
-                    if (Vector3.DistanceSquared(tar.Position, ObjectManager.Player.Position) > 630*630 &&
-                        disafter < 630*630)
-                    {
-                        Q.Cast(Game.CursorPos);
-                    }
-                }
-                //Q.Cast(Game.CursorPos);
+                Orbwalking.Orbwalk(TargetSelector.GetTarget(625, TargetSelector.DamageType.Physical), Game.CursorPos);
             }
+            if (Itemsmenu.Item("BOTRK").GetValue<bool>() && ((tar.Health / tar.MaxHealth) < botrk.Item("theirhp").GetValue<Slider>().Value) && ((ObjectManager.Player.Health / ObjectManager.Player.MaxHealth) < botrk.Item("myhp").GetValue<Slider>().Value))
+            {
+                Game.PrintChat("in");
+                if (Items.CanUseItem(3153))
+                {
+                    Items.UseItem(3153, tar);
+                }
+            }
+
+            if (Itemsmenu.Item("Ghostblade").GetValue<bool>())
+            {
+                if (Items.CanUseItem(3142))
+                {
+                    Items.UseItem(3142);
+                }
+            }
+            
+            if (menu.Item("UseEaa").GetValue<KeyBind>().Active)
+            {
+                E.Cast((Obj_AI_Base)target);
+                menu.Item("UseEaa").SetValue<KeyBind>(new KeyBind("G".ToCharArray()[0], KeyBindType.Toggle));
+            }
+
+            if (((orbwalker.ActiveMode.ToString() != "Combo" || !menu.Item("UseQC").GetValue<bool>()) &&
+                 ((orbwalker.ActiveMode.ToString() != "Mixed" || !menu.Item("hq").GetValue<bool>()) || !Q.IsReady())))
+                return;
+            var after = ObjectManager.Player.Position +
+                        Normalize(Game.CursorPos - ObjectManager.Player.Position) * 300;
+            //Game.PrintChat("After: {0}", after);
+            var disafter = Vector3.DistanceSquared(after, tar.Position);
+            //Game.PrintChat("DisAfter: {0}", disafter);
+            //Game.PrintChat("first calc: {0}", (disafter) - (630*630));
+            if ((disafter < 630 * 630) && disafter > 150 * 150)
+            {
+                Q.Cast(Game.CursorPos);
+            }
+            if (Vector3.DistanceSquared(tar.Position, ObjectManager.Player.Position) > 630 * 630 &&
+                disafter < 630 * 630)
+            {
+                Q.Cast(Game.CursorPos);
+            }
+            //Q.Cast(Game.CursorPos);
         }
 
         public static Vector3 Normalize(Vector3 A)
         {
-            double distance = Math.Sqrt(A.X*A.X + A.Y*A.Y);
-            return new Vector3(new Vector2((float) (A.X/distance)), (float) (A.Y/distance));
+            double distance = Math.Sqrt(A.X * A.X + A.Y * A.Y);
+            return new Vector3(new Vector2((float)(A.X / distance)), (float)(A.Y / distance));
         }
 
         public static void Game_OnGameUpdate(EventArgs args)
         {
-            if (!E.IsReady()) return; //||
-                //(orbwalker.ActiveMode.ToString() != "Combo" || !menu.Item("UseEC").GetValue<bool>()) &&
-                 //!menu.Item("UseET").GetValue<KeyBind>().Active)) return;
-            if (((orbwalker.ActiveMode.ToString() == "Combo" && menu.Item("UseEC").GetValue<bool>()) || (orbwalker.ActiveMode.ToString() == "Mixed" && menu.Item("he").GetValue<bool>()) || menu.Item("UseET").GetValue<KeyBind>().Active))
-            foreach (var hero in from hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(550f))
-                let prediction = E.GetPrediction(hero)
-                where NavMesh.GetCollisionFlags(
-                    prediction.UnitPosition.To2D()
-                        .Extend(ObjectManager.Player.ServerPosition.To2D(),
-                            -menu.Item("PushDistance").GetValue<Slider>().Value)
-                        .To3D())
-                    .HasFlag(CollisionFlags.Wall) || NavMesh.GetCollisionFlags(
-                        prediction.UnitPosition.To2D()
-                            .Extend(ObjectManager.Player.ServerPosition.To2D(),
-                                -(menu.Item("PushDistance").GetValue<Slider>().Value/2))
-                            .To3D())
-                        .HasFlag(CollisionFlags.Wall)
-                select hero)
+            if (menu.Item("aaqaa").GetValue<KeyBind>().Active)
             {
-                E.Cast(hero);
+                Orbwalking.Orbwalk(TargetSelector.GetTarget(625, TargetSelector.DamageType.Physical),Game.CursorPos);
             }
+            if (Itemsmenu.Item("QSS").GetValue<bool>())
+            {
+                for (int i = 0; i < buffs.Length; i++)
+                {
+                    if (ObjectManager.Player.HasBuffOfType(buffs[i]) && qss.Item(buffs[i].ToString()).GetValue<bool>())
+                    {
+                        if (Items.CanUseItem(3140))
+                        {
+                            Items.UseItem(3140);
+                        }
+                        else if (Items.CanUseItem(3139))
+                        {
+                            Items.UseItem(3140);
+                        }
+                        else if (cleanse != null && cleanse.IsReady())
+                        {
+                            cleanse.Cast();
+                        }
+                    }
+                }
+            }
+            if (!E.IsReady()) return; //||
+            //(orbwalker.ActiveMode.ToString() != "Combo" || !menu.Item("UseEC").GetValue<bool>()) &&
+            //!menu.Item("UseET").GetValue<KeyBind>().Active)) return;
+            if (((orbwalker.ActiveMode.ToString() == "Combo" && menu.Item("UseEC").GetValue<bool>()) || (orbwalker.ActiveMode.ToString() == "Mixed" && menu.Item("he").GetValue<bool>()) || menu.Item("UseET").GetValue<KeyBind>().Active))
+                foreach (var hero in from hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(550f))
+                                     let prediction = E.GetPrediction(hero)
+                                     where NavMesh.GetCollisionFlags(
+                                         prediction.UnitPosition.To2D()
+                                             .Extend(ObjectManager.Player.ServerPosition.To2D(),
+                                                 -menu.Item("PushDistance").GetValue<Slider>().Value)
+                                             .To3D())
+                                         .HasFlag(CollisionFlags.Wall) || NavMesh.GetCollisionFlags(
+                                             prediction.UnitPosition.To2D()
+                                                 .Extend(ObjectManager.Player.ServerPosition.To2D(),
+                                                     -(menu.Item("PushDistance").GetValue<Slider>().Value / 2))
+                                                 .To3D())
+                                             .HasFlag(CollisionFlags.Wall)
+                                     select hero)
+                {
+                    E.Cast(hero);
+                }
         }
     }
 }
--- hi im gosu/hi im gosu/Properties/AssemblyInfo.cs
+++ hi im gosu/hi im gosu/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("hi im gosu")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fb564d7c-408b-4be8-915b-ba02f333e384")]
+[assembly: Guid("aa1e94c6-6879-4dbe-a740-18d5c9aad955")]
 
 // Version information for an assembly consists of the following four values:
 //
