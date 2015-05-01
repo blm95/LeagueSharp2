644e8fea682e978fe626e0d9f584673ca475c603
blm95 <blm95@comcast.net>
2015-04-30 22:16:08 -0400

    Added position sliders

--- BuildOptimizer/BuildOptimizer/Program.cs
+++ BuildOptimizer/BuildOptimizer/Program.cs
@@ -25,6 +25,8 @@ namespace BuildOptimizer
         private static bool MRalert = false;
         private static bool Armoralert = false;
 
+        private static Menu menu;
+
         static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
@@ -32,6 +34,26 @@ namespace BuildOptimizer
 
         static void Game_OnGameLoad(EventArgs args)
         {
+            menu = new Menu("Build Optimizer","BO",true);
+
+            menu.AddItem(new MenuItem("ADX", "AD display position X")).SetValue(new Slider(1300, 0, 2500));
+            menu.AddItem(new MenuItem("ADY", "AD display position Y")).SetValue(new Slider(940, 0, 2500));
+
+            menu.AddItem(new MenuItem("APX", "AP display position X")).SetValue(new Slider(1300, 0, 2500));
+            menu.AddItem(new MenuItem("APY", "AP display position Y")).SetValue(new Slider(960, 0, 2500));
+
+            menu.AddItem(new MenuItem("ArmorX", "Armor display position X")).SetValue(new Slider(1300, 0, 2500));
+            menu.AddItem(new MenuItem("ArmorY", "Armor display position Y")).SetValue(new Slider(980, 0, 2500));
+
+            menu.AddItem(new MenuItem("MRX", "MR display position X")).SetValue(new Slider(1300, 0, 2500));
+            menu.AddItem(new MenuItem("MRY", "MR display position Y")).SetValue(new Slider(1000, 0, 2500));
+
+            menu.AddItem(new MenuItem("badArmorX", "Armor inaccuracy X")).SetValue(new Slider(1480, 0, 2500));
+            menu.AddItem(new MenuItem("badArmorY", "Armor inaccuracy Y")).SetValue(new Slider(980, 0, 2500));
+
+            menu.AddItem(new MenuItem("badMRX", "MR inaccuracy X")).SetValue(new Slider(1480, 0, 2500));
+            menu.AddItem(new MenuItem("badMRY", "MR inaccuracy Y")).SetValue(new Slider(1000, 0, 2500));
+
             foreach (var k in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsEnemy))
             {
                 ADstats.Add(k.ChampionName,k.TotalAttackDamage);
@@ -39,6 +61,9 @@ namespace BuildOptimizer
                 ArmorStats.Add(k.ChampionName,k.Armor);
                 MRStats.Add(k.ChampionName,k.SpellBlock);
             }
+
+            menu.AddToMainMenu();
+
             Drawing.OnDraw += Drawing_OnDraw;
         }
 
@@ -108,21 +133,21 @@ namespace BuildOptimizer
                 if (k > totalMR / 3)
                 {
                     MRalert = true;
-                }
+                }  
             }
 
             //var k = Drawing.WorldToScreen(new Vector3(960, 540, 0));
-            Drawing.DrawText(1300, 940, System.Drawing.Color.Red, "Total Enemy AD: " + (int)totalAD);
-            Drawing.DrawText(1300, 960, System.Drawing.Color.LightSkyBlue, "Total Enemy AP: " + (int)totalAP);
-            Drawing.DrawText(1300, 980, System.Drawing.Color.DarkOrange, "Total Enemy Armor: " + (int)totalArmor);
+            Drawing.DrawText(menu.Item("ADX").GetValue<Slider>().Value, menu.Item("ADY").GetValue<Slider>().Value, System.Drawing.Color.Red, "Total Enemy AD: " + (int)totalAD);
+            Drawing.DrawText(menu.Item("APX").GetValue<Slider>().Value, menu.Item("APY").GetValue<Slider>().Value, System.Drawing.Color.LightSkyBlue, "Total Enemy AP: " + (int)totalAP);
+            Drawing.DrawText(menu.Item("ArmorX").GetValue<Slider>().Value, menu.Item("ArmorY").GetValue<Slider>().Value, System.Drawing.Color.DarkOrange, "Total Enemy Armor: " + (int)totalArmor);
             if (Armoralert)
             {
-                Drawing.DrawText(1480, 980, System.Drawing.Color.Red, "** not accurate"); 
+                Drawing.DrawText(menu.Item("badArmorX").GetValue<Slider>().Value, menu.Item("badArmorY").GetValue<Slider>().Value, System.Drawing.Color.Red, "** not accurate"); 
             }
-            Drawing.DrawText(1300, 1000, System.Drawing.Color.SpringGreen, "Total Enemy MR: " + (int)totalMR);
+            Drawing.DrawText(menu.Item("MRX").GetValue<Slider>().Value, menu.Item("MRY").GetValue<Slider>().Value, System.Drawing.Color.SpringGreen, "Total Enemy MR: " + (int)totalMR);
             if (MRalert)
             {
-                Drawing.DrawText(1480, 1000, System.Drawing.Color.Red, "** not accurate");
+                Drawing.DrawText(menu.Item("badMRX").GetValue<Slider>().Value, menu.Item("badMRY").GetValue<Slider>().Value, System.Drawing.Color.Red, "** not accurate");
             }
         }
     }
